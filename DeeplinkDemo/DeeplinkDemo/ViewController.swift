be2942f27943c3664a9d8af33970eea4df40cd4d
arimura <kotaro_arimura@voyagegroup.com>
2022-04-19 20:29:09 +0900

    Add URL scheme button

--- DeeplinkDemo/DeeplinkDemo/ViewController.swift
+++ DeeplinkDemo/DeeplinkDemo/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlSchemeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func tapUrlScheme(_ sender: Any) {
+        print("hoge")
+    }
 }
+
 
