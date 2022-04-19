ab1a2d79c2228fea1e6b5766791b8fe84df68e83
arimura <kotaro_arimura@voyagegroup.com>
2022-04-19 20:17:18 +0900

    Init

---
+++ DeeplinkDemo/DeeplinkDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeeplinkDemo
+//
+//  Created by k-arimura on 2022/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
